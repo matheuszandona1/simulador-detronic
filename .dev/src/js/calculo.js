44f0a0b245f821bc999b9fa2ca2c13eb157e18e9
diaslol <mastronelli88@gmail.com>
2023-05-05 10:23:18 -0300

    ✨ planos

--- .dev/src/js/calculo.js
+++ .dev/src/js/calculo.js
@@ -25,6 +25,8 @@ function calcularDesconto() {
 			tarifaIluminacao: 30,
 			tarifaRede: 100,
 			descontoMax: 0.85,
+			descontoMed: 0.88,
+			descontoMin: 0.9,
 		},
 	}
 	const valorConta = parseFloat(document.querySelector("#valor").value)
@@ -33,25 +35,27 @@ function calcularDesconto() {
 	const consumoKWH =  consumoReal /  tarifas[tipoConta].tarifaCheia
 	const valorEnergiaDesconto = consumoKWH - tarifas[tipoConta].tarifaRede
 	const valorConsumoDesconto = valorEnergiaDesconto * tarifas[tipoConta].tarifaDesconto * tarifas[tipoConta].descontoMax
+	const valorConsumoDescontoMed = valorEnergiaDesconto * tarifas[tipoConta].tarifaDesconto * tarifas[tipoConta].descontoMed
+	const valorConsumoDescontoMin = valorEnergiaDesconto * tarifas[tipoConta].tarifaDesconto * tarifas[tipoConta].descontoMin
+
 	
 	const valorPisCofins = tarifas[tipoConta].tarifaPisCofins * valorEnergiaDesconto
 	const valorCustoDisponibilidade = tarifas[tipoConta].tarifaRede * tarifas[tipoConta].tarifaCheia
 
 	const valorFinalCusto =  valorConsumoDesconto + valorPisCofins + valorCustoDisponibilidade + tarifas[tipoConta].tarifaIluminacao
-	const valorFinalDescontoMensal = valorConta - valorFinalCusto
+	const valorFinalCustoMed =  valorConsumoDescontoMed + valorPisCofins + valorCustoDisponibilidade + tarifas[tipoConta].tarifaIluminacao
+	const valorFinalCustoMin =  valorConsumoDescontoMin + valorPisCofins + valorCustoDisponibilidade + tarifas[tipoConta].tarifaIluminacao
 
-	// console.log('valorConta', valorConta)
-	// console.log("consumoReal", consumoReal)
-	// console.log("consumoKWH", consumoKWH)
-	// console.log("valorEnergiaDesconto", valorEnergiaDesconto)
-	// console.log("valorConsumoDesconto", valorConsumoDesconto)
-	// console.log("valorPisCofins", valorPisCofins)
-	// console.log("valorCustoDisponibilidade", valorCustoDisponibilidade)
-	// console.log(valorFinalCusto)
-	// console.log(valorFinalDescontoMensal)
+	const valorFinalDescontoMensal = valorConta - valorFinalCusto
+	const valorFinalDescontoMensalMed = valorConta - valorFinalCustoMed
+	const valorFinalDescontoMensalMin = valorConta - valorFinalCustoMin
+
 
 	localStorage.setItem("valorConta", valorConta.toFixed(2))
 	localStorage.setItem("valorFinalDescontoMensal", valorFinalDescontoMensal.toFixed(2))
+	localStorage.setItem("valorFinalDescontoMensalMed", valorFinalDescontoMensalMed.toFixed(2))
+	localStorage.setItem("valorFinalDescontoMensalMin", valorFinalDescontoMensalMin.toFixed(2))
+
 	localStorage.setItem("tipoConta", tipoConta)
 }
 
